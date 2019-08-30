587ac3b72261bd0da1042cecd5356d130685bfb4
lgrn <linus@agren.cc>
2019-08-30 18:40:26 +0200

    Provision script changes
    - Use curl instead of wget, no point in adding an extra dep
    - Hopefully clarified some messages regarding what's going on

--- provision.sh
+++ provision.sh
@@ -3,86 +3,84 @@
 timedatectl set-timezone Europe/Stockholm # timedatectl list-timezones
 timedatectl --adjust-system-clock
 
-# wget: may not be included
+# curl: may not be included
 
-echo "[>>>] Installing wget, vim and mlocate."
-yum install wget vim mlocate -y -q &>/dev/null
+echo "[>>>] Installing curl, vim and mlocate."
+yum install vim mlocate -y -q &>/dev/null
 
 ### MANUAL VERSION SELECTION BELOW -- NOT REQUIRED! LATEST VERSION WILL BE GRABBED
 ### If no file is grabbed, "latest" is grabbed automatically later.
 
 cd /vagrant
-OP5URL='--quiet https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive'
+OP5URL='--remote-name --silent https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive'
 
-echo "[>>>] In case you chose to wget a file, it will be downloaded now."
-# Uncomment a wget line if you want the version specified to replace 'latest':
+echo "[>>>] In case you chose to curl a file, it will be downloaded now."
+# Uncomment a curl line if you want the version specified to replace 'latest':
 
-# wget $OP5URL/Monitor8/Tarball/op5-monitor-8.0.6-x64.tar.gz
-# wget $OP5URL/Monitor8/Tarball/op5-monitor-8.0.5-x64.tar.gz
-# wget $OP5URL/Monitor8/Tarball/op5-monitor-8.0.4-x64.tar.gz
-# wget $OP5URL/Monitor8/Tarball/op5-monitor-8.0.3-x64.tar.gz
-# wget $OP5URL/Monitor8/Tarball/op5-monitor-8.0.2-x64.tar.gz
-# wget $OP5URL/Monitor8/Tarball/op5-monitor-8.0.1-x64.tar.gz
-############## note: naming scheme changed >8.0.1 ##########
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.6-x64.tar.gz
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.5-x64.tar.gz
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.4-x64.tar.gz
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.3-x64.tar.gz
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.2-x64.tar.gz
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.1-x64.tar.gz
+############## note: naming scheme changed >8.0.0 ##########
 
-# wget $OP5URL/Monitor8/Tarball/op5-monitor-8.0.0.x64.tar.gz
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.0.x64.tar.gz
 
-# wget $OP5URL/op5-monitor-7.5.2.x64.tar.gz
-# wget $OP5URL/op5-monitor-7.5.1.x64.tar.gz
-# wget $OP5URL/op5-monitor-7.5.0.x64.tar.gz
-# wget $OP5URL/op5-monitor-7.4.11.x64.tar.gz
-# wget $OP5URL/op5-monitor-7.4.6.x64.tar.gz
-# wget $OP5URL/op5-monitor-7.4.5-20180806.tar.gz
-# wget $OP5URL/op5-monitor-7.4.3-20180612.tar.gz
-# wget $OP5URL/OP5-Monitor-7.4.4-20180711.tar.gz
-# wget $OP5URL/op5-monitor-7.4.2-20180515.tar.gz
-# wget $OP5URL/op5-monitor-7.4.1-20180420.tar.gz
-# wget $OP5URL/op5-monitor-7.4.0-20180320.tar.gz
-# wget $OP5URL/op5-monitor-7.3.21-20180226.tar.gz
-# wget $OP5URL/op5-monitor-7.3.20-20180124.tar.gz
-# wget $OP5URL/op5-monitor-7.3.20-20180124.tar.gz
-# wget $OP5URL/op5-monitor-7.3.19-20171212.tar.gz
-# wget $OP5URL/op5-monitor-7.3.2-20161114.tar.gz
-# wget $OP5URL/op5-monitor-7.0.0-20140903.tar.gz
+# curl $OP5URL/op5-monitor-7.5.2.x64.tar.gz
+# curl $OP5URL/op5-monitor-7.5.1.x64.tar.gz
+# curl $OP5URL/op5-monitor-7.5.0.x64.tar.gz
+# curl $OP5URL/op5-monitor-7.4.11.x64.tar.gz
+# curl $OP5URL/op5-monitor-7.4.6.x64.tar.gz
+# curl $OP5URL/op5-monitor-7.4.5-20180806.tar.gz
+# curl $OP5URL/op5-monitor-7.4.3-20180612.tar.gz
+# curl $OP5URL/OP5-Monitor-7.4.4-20180711.tar.gz
+# curl $OP5URL/op5-monitor-7.4.2-20180515.tar.gz
+# curl $OP5URL/op5-monitor-7.4.1-20180420.tar.gz
+# curl $OP5URL/op5-monitor-7.4.0-20180320.tar.gz
+# curl $OP5URL/op5-monitor-7.3.21-20180226.tar.gz
+# curl $OP5URL/op5-monitor-7.3.20-20180124.tar.gz
+# curl $OP5URL/op5-monitor-7.3.20-20180124.tar.gz
+# curl $OP5URL/op5-monitor-7.3.19-20171212.tar.gz
+# curl $OP5URL/op5-monitor-7.3.2-20161114.tar.gz
+# curl $OP5URL/op5-monitor-7.0.0-20140903.tar.gz
 
 # If a version is missing above, the filename may be available from:
 # https://www.op5.com/download/archives/ ("Version" dropdown)
 
 ### UNSUPPORTED VERSIONS ###
 
-# wget $OP5URL/op5-monitor-6.3.3-20140912.tar.gz
+# curl $OP5URL/op5-monitor-6.3.3-20140912.tar.gz
 
 ### NO CHANGES BELOW PLEASE ###
 
 if [ ! -f /vagrant/*onitor*gz ]; then
-    echo "[>>>] Didn't find any monitor file in /vagrant -- you probably didn't supply one."
-	echo "[>>>] Grabbing Monitor 8 instead from the information superhighway."
+    echo "[>>>] No monitor file in /vagrant -- you didn't place one, or didn't choose a specific version."
+	echo "[>>>] Since there's no installation file, we're grabbing the latest Monitor 8 instead."
     cd /tmp && curl -O https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive/Monitor8/Tarball/op5-monitor-8.0.6-x64.tar.gz &>/dev/null && tar xvf *.gz &>/dev/null
-    echo "[>>>] Whoosh! Download and unpack complete. Running non-interactive installation script. This will take some time."
+    echo "[>>>] Download and unpack is complete. Now executing non-interactive OP5 installation script. Expect no output: this will take some time."
     cd *onitor*/ && ./install.sh --noninteractive &>/dev/null
 else
-    echo "[>>>] There's already an op5-monitor file in /vagrant -- most likely because you put it there. It will be used."
+    echo "[>>>] op5-monitor file was found in /vagrant, using that to install."
     cd /vagrant && tar xvf *onitor*.gz &>/dev/null
-    echo "[>>>] That file is now unpacked. Running non-interactive installation script. This will take some time."
+    echo "[>>>] Download and unpack is complete. Now executing non-interactive OP5 installation script. Expect no output: this will take some time."
     cd *onitor*/ && ./install.sh --noninteractive &>/dev/null
 fi
 
 echo -e "\n\n"
-# Any .lic files in /vagrant should be placed:
 
+# Any .lic file in /vagrant ? If so, use it.
 echo "[>>>] If a license file exists, it will now be moved into place."
 mv -v /vagrant/*.lic /etc/op5license/
 
-echo "[>>>] Showing you time remaining for your OP5 license:"
+echo "[>>>] Time remaining for your OP5 license (check_op5_license):"
 /opt/plugins/check_op5_license -w1 -c1 -T d
 
+echo "[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]"
 echo "[>>>] The provision script for this guest has finished."
 echo "[>>>] You should be able to access this Monitor instance on:"
 echo "[>>>] https://localhost:4436 (Centos6) or https://localhost:4437 (Centos7)."
-echo "[>>>] For more information on guest configuration, see the vagraint file. Have fun!"
+echo "[>>>] For more information on guest configuration, see the vagrant file. Have fun!"
 echo "[!!!] If Monitor/Apache/etc needs root, you may want to take note of /etc/pam.d/su "
-echo "[!!!] On some boxes, only the 'vagrant' user is allowed to sudo. cat of file:"
-echo "[***]"
-cat /etc/pam.d/su
-echo "[***]"
-echo "[>>>] Provisioning done."
+echo "[!!!] On some boxes, only the 'vagrant' user is allowed to sudo."
+echo "[>>>] DONE. OP5 Vagrant script signing off. Good night."
