90aa404e909c9ad3aa597aff848acba22a93262d
lgrn <linus@agren.cc>
2019-08-30 18:54:09 +0200

    Polishing output

--- provision.sh
+++ provision.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
+OP5VV='0.1'
+
 timedatectl set-timezone Europe/Stockholm # timedatectl list-timezones
 timedatectl --adjust-system-clock
 
-# curl: may not be included
-
-echo "[>>>] Installing curl, vim and mlocate."
+echo "[>>$OP5VV] Installing vim and mlocate."
 yum install vim mlocate -y -q &>/dev/null
 
-### MANUAL VERSION SELECTION BELOW -- NOT REQUIRED! LATEST VERSION WILL BE GRABBED
-### If no file is grabbed, "latest" is grabbed automatically later.
+### MANUAL VERSION SELECTION BELOW -- NOT REQUIRED!
+### IF YOU DON'T UNCOMMENT A LINE, THE LATEST VERSION WILL BE GRABBED
 
 cd /vagrant
 OP5URL='--remote-name --silent https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive'
 
-echo "[>>>] In case you chose to curl a file, it will be downloaded now."
+echo "[>>$OP5VV] In case you chose to curl a file, it will be downloaded now."
+
 # Uncomment a curl line if you want the version specified to replace 'latest':
 
 # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.6-x64.tar.gz
@@ -52,35 +53,42 @@ echo "[>>>] In case you chose to curl a file, it will be downloaded now."
 
 # curl $OP5URL/op5-monitor-6.3.3-20140912.tar.gz
 
-### NO CHANGES BELOW PLEASE ###
+### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ###
+###                                          NO CHANGES BELOW PLEASE                                         ###
+### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ###
+
 
 if [ ! -f /vagrant/*onitor*gz ]; then
-    echo "[>>>] No monitor file in /vagrant -- you didn't place one, or didn't choose a specific version."
-	echo "[>>>] Since there's no installation file, we're grabbing the latest Monitor 8 instead."
-    cd /tmp && curl -O https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive/Monitor8/Tarball/op5-monitor-8.0.6-x64.tar.gz &>/dev/null && tar xvf *.gz &>/dev/null
-    echo "[>>>] Download and unpack is complete. Now executing non-interactive OP5 installation script. Expect no output: this will take some time."
+    echo "[>>$OP5VV] No monitor file in /vagrant -- you didn't place one, or didn't choose a specific version."
+	echo "[>>$OP5VV] Since there's no installation file, we're grabbing the latest Monitor 8 instead."
+
+    $LATEST_FILENAME='op5-monitor-8.0.6-x64.tar.gz'
+
+    echo "[>>$OP5VV] Now grabbing and unpacking: $LATEST_FILENAME (default)"
+    cd /tmp && curl -O https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive/Monitor8/Tarball/$LATEST_FILENAME &>/dev/null && tar xvf *.gz &>/dev/null
+    echo "[>>$OP5VV] Download and unpack is complete. Now executing non-interactive OP5 installation script. Expect no output: this will take some time."
     cd *onitor*/ && ./install.sh --noninteractive &>/dev/null
 else
-    echo "[>>>] op5-monitor file was found in /vagrant, using that to install."
+    echo "[>>$OP5VV] op5-monitor file was found in /vagrant, using that to install."
     cd /vagrant && tar xvf *onitor*.gz &>/dev/null
-    echo "[>>>] Download and unpack is complete. Now executing non-interactive OP5 installation script. Expect no output: this will take some time."
+    echo "[>>$OP5VV] Download and unpack is complete. Now executing non-interactive OP5 installation script. Expect no output: this will take some time."
     cd *onitor*/ && ./install.sh --noninteractive &>/dev/null
 fi
 
 echo -e "\n\n"
 
 # Any .lic file in /vagrant ? If so, use it.
-echo "[>>>] If a license file exists, it will now be moved into place."
+echo "[>>$OP5VV] If a license file exists, it will now be moved into place."
 mv -v /vagrant/*.lic /etc/op5license/
 
-echo "[>>>] Time remaining for your OP5 license (check_op5_license):"
+echo "[>>$OP5VV] Time remaining for your OP5 license (check_op5_license):"
 /opt/plugins/check_op5_license -w1 -c1 -T d
 
-echo "[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]"
-echo "[>>>] The provision script for this guest has finished."
-echo "[>>>] You should be able to access this Monitor instance on:"
-echo "[>>>] https://localhost:4436 (Centos6) or https://localhost:4437 (Centos7)."
-echo "[>>>] For more information on guest configuration, see the vagrant file. Have fun!"
-echo "[!!!] If Monitor/Apache/etc needs root, you may want to take note of /etc/pam.d/su "
-echo "[!!!] On some boxes, only the 'vagrant' user is allowed to sudo."
-echo "[>>>] DONE. OP5 Vagrant script signing off. Good night."
+echo "[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]"
+echo "[>>$OP5VV] The provision script for this guest has finished."
+echo "[>>$OP5VV] You should be able to access this Monitor instance on:"
+echo "[>>$OP5VV] https://localhost:4436 (Centos6) or https://localhost:4437 (Centos7)."
+echo "[>>$OP5VV] For more information on guest configuration, see the vagrant file. Have fun!"
+echo "[!!$OP5VV] If Monitor/Apache/etc needs root, you may want to take note of /etc/pam.d/su "
+echo "[!!$OP5VV] On some boxes, only the 'vagrant' user is allowed to sudo."
+echo "[>>$OP5VV] DONE. OP5 Vagrant script signing off. Good night."
