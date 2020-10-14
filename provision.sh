5561cc586aa1ca1e1233ccce57e4f4e551638020
Linus <linus@agren.cc>
2020-10-14 11:56:15 +0200

    New versions

--- provision.sh
+++ provision.sh
@@ -19,6 +19,9 @@ echo $($TIMES) $PREFX "If you chose to curl a file, it will be downloaded now."
 
 # TODO: Having a list for this is unmaintainable, a flag should take the version number.
 
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.3-x64.tar.gz
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.2-x64.tar.gz
+# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.1-x64.tar.gz
 # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.0-x64.tar.gz
 # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.4-x64.tar.gz
 # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.3-x64.tar.gz
@@ -38,6 +41,8 @@ echo $($TIMES) $PREFX "If you chose to curl a file, it will be downloaded now."
 
 # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.0.x64.tar.gz
 
+# curl $OP5URL/op5-monitor-7.5.13.x64.tar.gz
+# curl $OP5URL/op5-monitor-7.5.12.x64.tar.gz
 # curl $OP5URL/op5-monitor-7.5.11.x64.tar.gz
 # curl $OP5URL/op5-monitor-7.5.10.x64.tar.gz
 # curl $OP5URL/op5-monitor-7.5.9.x64.tar.gz
@@ -82,7 +87,7 @@ if [ ! -f /vagrant/*onitor*gz ]; then
     echo $($TIMES) $PREFX "You didn't choose a specific version to install, /vagrant is empty."
 	echo $($TIMES) $PREFX "Since there's no installation file, we're grabbing the latest Monitor 8 release."
 
-    LATEST_FILENAME='op5-monitor-8.2.0-x64.tar.gz'
+    LATEST_FILENAME='op5-monitor-8.2.3-x64.tar.gz'
 
     echo $($TIMES) $PREFX "Latest version is assumed to be: $LATEST_FILENAME (default)"
     cd /tmp && curl -O https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive/Monitor8/Tarball/$LATEST_FILENAME &>/dev/null && tar xvf *.gz &>/dev/null
