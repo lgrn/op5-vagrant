6d89199d54702a1c9e12b7fc5841c63c1245c3a2
Linus <linus@agren.cc>
2020-11-05 21:28:15 +0100

    v0.3: Flags, flags, flags!
    Lots of nice new features, available to you via args in the Vagrantfile.
    
    Changelog:
    
    - provision.sh now requires the -r flag to run at all, otherwise usage info
      will be printed instead. It's the only mandatory flag.
    
    - use the -v flag for verbose output if you're curious what's going on, if it's
      not provided most of the process will be silent. Logging has been improved.
    
    - use the -m flag to specify a Monitor version to install. This future-safes
      the script a bit since it doesn't require a list of available versions. This
      only works for versions >8.0.0
    
    - use the -p flag to automatically install xdebug and set the vagrant ssh key
      for root login. When done, you should be able to easily connect VSCode or
      another IDE to the machine over SSH and debug PHP during runtime.
    
    - the centos6 box has been commented out and marked as unsupported.
    
    - you might see HSTS warnings in some browsers, so a warning regarding this
      has been added at the end of the script.

--- Vagrantfile
+++ Vagrantfile
@@ -6,23 +6,23 @@ Vagrant.configure("2") do |config|
   config.vm.provider "virtualbox" do |vb|
     vb.memory = "2048"
   end
-  
-  config.vm.define "centos6" do |centos6|
-    centos6.vm.box = "centos/6"
-    centos6.vm.hostname = %x(./namkaran -d EL6).chomp
-    centos6.vm.network "forwarded_port", guest: 443, host: 4436
-    centos6.vm.network "private_network", type: "dhcp"
-    centos6.vm.provision "shell", path: "provision.sh"
-  end
+
+### UNSUPPORTED ###  
+#  config.vm.define "centos6" do |centos6|
+#    centos6.vm.box = "centos/6"
+#    centos6.vm.hostname = %x(./namkaran -d EL6).chomp
+#    centos6.vm.network "forwarded_port", guest: 443, host: 4436
+#    centos6.vm.network "private_network", type: "dhcp"
+#    centos6.vm.provision "shell", path: "provision.sh"
+#  end
   
   config.vm.define "centos7" do |centos7|
     centos7.vm.box = "centos/7"
     centos7.vm.hostname = %x(./namkaran -d EL7).chomp
     centos7.vm.network "forwarded_port", guest: 443, host: 4437
     centos7.vm.network "private_network", type: "dhcp"
-    centos7.vm.provision "shell", path: "provision.sh"
+    centos7.vm.provision "shell", path: "provision.sh", :args => "-r -v -p -m 8.2.3"
   end
-  
 end
 
 # Load user extras
--- provision.sh
+++ provision.sh
@@ -1,131 +1,258 @@
 #!/usr/bin/env bash
+PREFX="[v0.3]"
 
-OP5VV="0.2"
-TIMES='date +%T'
-PREFX="[v.$OP5VV]"
-timedatectl set-timezone Europe/Stockholm &>/dev/null # timedatectl list-timezones
-timedatectl --adjust-system-clock &> /dev/null
+usage()
+{
+    echo "Usage: provision.sh <-r|-p|-v|-t|-m>"
+    echo "  -r : Run the script."
+    echo "  -p : Install libraries necessary for PHP debugging."
+    echo "  -v : Provide verbose output, script is quite silent by default."
+    echo "  -m : Supply a Monitor version to download, \
+example: '8.2.3' (8+ only!)"
+}
+
+log()
+{
+    TIMES='date +%T'
+    [[ "$verbose" == "true" ]] && echo "$($TIMES)" "$PREFX" "$1"
+}
+
+check_for_monitor_file()
+{
+fileexists="false"
+for filecheck in /vagrant/*onitor*gz 
+do
+    if [ -e "$filecheck" ]
+    then
+        fileexists="true"
+        break
+    fi
+done
+}
+
+while getopts "rpvtm:" flag; do
+  case ${flag} in
+    r) run='true' ;;
+    p) phpdebug='true' ;;
+    v) verbose='true' ;;
+    m) monversion=$OPTARG ;;
+    *) error "Unexpected option." ;;
+  esac
+done
+
+[[ "$run" != "true" ]] && { usage; log "Missing -r flag."; exit 1; }
+
+log "Setting default timezone (Europe/Stockholm)."
+timedatectl set-timezone Europe/Stockholm &>/dev/null
 ln -fs /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 
-### MANUAL VERSION SELECTION BELOW -- NOT REQUIRED!
-### IF YOU DON'T UNCOMMENT A LINE, THE LATEST VERSION WILL BE GRABBED
+log "Adjusting system clock."
+timedatectl --adjust-system-clock > /dev/null
 
-cd /vagrant
-OP5URL='--remote-name https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive'
+log "TIP: If downloads are slow, place the tar.gz in your vagrantdir beforehand."
 
-echo $($TIMES) $PREFX "If you chose to curl a file, it will be downloaded now."
+if [[ -n $monversion ]]; then
+    monurl="https://d2ubxhm80y3bwr.cloudfront.net/Downloads/\
+op5_monitor_archive/Monitor8/Tarball/op5-monitor-$monversion-x64.tar.gz"
 
-# Uncomment a curl line if you want the version specified to replace 'latest':
-
-# TODO: Having a list for this is unmaintainable, a flag should take the version number.
-
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.3-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.2-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.1-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.0-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.4-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.3-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.2-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.1-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.0-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.9-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.8-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.7-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.6-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.5-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.4-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.3-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.2-x64.tar.gz
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.1-x64.tar.gz
-############## note: naming scheme changed >8.0.0 ##########
-
-# curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.0.x64.tar.gz
-
-# curl $OP5URL/op5-monitor-7.5.13.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.12.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.11.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.10.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.9.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.8.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.7.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.6.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.5.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.4.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.3.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.2.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.1.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.5.0.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.4.11.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.4.6.x64.tar.gz
-# curl $OP5URL/op5-monitor-7.4.5-20180806.tar.gz
-# curl $OP5URL/op5-monitor-7.4.3-20180612.tar.gz
-# curl $OP5URL/OP5-Monitor-7.4.4-20180711.tar.gz
-# curl $OP5URL/op5-monitor-7.4.2-20180515.tar.gz
-# curl $OP5URL/op5-monitor-7.4.1-20180420.tar.gz
-# curl $OP5URL/op5-monitor-7.4.0-20180320.tar.gz
-# curl $OP5URL/op5-monitor-7.3.21-20180226.tar.gz
-# curl $OP5URL/op5-monitor-7.3.20-20180124.tar.gz
-# curl $OP5URL/op5-monitor-7.3.20-20180124.tar.gz
-# curl $OP5URL/op5-monitor-7.3.19-20171212.tar.gz
-# curl $OP5URL/op5-monitor-7.3.2-20161114.tar.gz
-# curl $OP5URL/op5-monitor-7.2.2-20160513.tar.gz
-# curl $OP5URL/op5-monitor-7.0.0-20140903.tar.gz
-
-# If a version is missing above, the filename may be available from:
-# https://resources.itrsgroup.com/downloads
-
-### UNSUPPORTED VERSIONS ###
-
-# curl $OP5URL/op5-monitor-6.3.3-20140912.tar.gz
-
-### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ###
-###                                          NO CHANGES BELOW PLEASE                                         ###
-### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ###
-
-
-if [ ! -f /vagrant/*onitor*gz ]; then
-    echo $($TIMES) $PREFX "You didn't choose a specific version to install, /vagrant is empty."
-	echo $($TIMES) $PREFX "Since there's no installation file, we're grabbing the latest Monitor 8 release."
-
-    LATEST_FILENAME='op5-monitor-8.2.3-x64.tar.gz'
-
-    echo $($TIMES) $PREFX "Latest version is assumed to be: $LATEST_FILENAME (default)"
-    cd /tmp && curl -O https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive/Monitor8/Tarball/$LATEST_FILENAME &>/dev/null && tar xvf *.gz &>/dev/null
-    echo $($TIMES) $PREFX "Unpack complete. Executing non-interactive OP5 installation. Expect no output, this will take some time."
-    cd *onitor*/ && ./install.sh --noninteractive &>/dev/null
+    if curl -O /dev/null --silent --head --fail "$monurl" &>/dev/null ; then
+        log "Downloading $monversion as provided."
+        cd /vagrant && curl -O "$monurl" &>/dev/null
+    else
+        log "Failed to find provided version: $monversion. Exiting."
+        exit 1
+    fi
 else
-    echo $($TIMES) $PREFX "You manually chose a specific version to install, so we're using that. Unpacking."
+    log "-m wasn't provided, falling back to \
+legacy mode (latest or provided file)."
+    
+    cd /vagrant || exit 1
+    
+    OP5URL="-O \
+https://d2ubxhm80y3bwr.cloudfront.net/Downloads/op5_monitor_archive"
+    LATEST_FILENAME='/Monitor8/Tarball/op5-monitor-8.2.3-x64.tar.gz'
+    
+    # Uncomment a curl line below to replace 'latest'.
+    #
+    # The way this works is that since it will place a file in the vagrantdir
+    # before the checks below run, it will act just like if you had placed it
+    # there manually: i.e. it detects a local file and uses that.
+
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.3-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.2-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.1-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.2.0-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.4-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.3-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.2-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.1-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.1.0-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.9-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.8-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.7-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.6-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.5-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.4-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.3-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.2-x64.tar.gz
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.1-x64.tar.gz
+    
+    ############## note: naming scheme changed >8.0.0 ##########
+
+    # curl $OP5URL/Monitor8/Tarball/op5-monitor-8.0.0.x64.tar.gz
+
+    # curl $OP5URL/op5-monitor-7.5.13.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.12.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.11.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.10.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.9.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.8.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.7.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.6.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.5.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.4.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.3.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.2.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.1.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.5.0.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.4.11.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.4.6.x64.tar.gz
+    # curl $OP5URL/op5-monitor-7.4.5-20180806.tar.gz
+    # curl $OP5URL/op5-monitor-7.4.3-20180612.tar.gz
+    # curl $OP5URL/OP5-Monitor-7.4.4-20180711.tar.gz
+    # curl $OP5URL/op5-monitor-7.4.2-20180515.tar.gz
+    # curl $OP5URL/op5-monitor-7.4.1-20180420.tar.gz
+    # curl $OP5URL/op5-monitor-7.4.0-20180320.tar.gz
+    # curl $OP5URL/op5-monitor-7.3.21-20180226.tar.gz
+    # curl $OP5URL/op5-monitor-7.3.20-20180124.tar.gz
+    # curl $OP5URL/op5-monitor-7.3.20-20180124.tar.gz
+    # curl $OP5URL/op5-monitor-7.3.19-20171212.tar.gz
+    # curl $OP5URL/op5-monitor-7.3.2-20161114.tar.gz
+    # curl $OP5URL/op5-monitor-7.2.2-20160513.tar.gz
+    # curl $OP5URL/op5-monitor-7.0.0-20140903.tar.gz
+
+    # If a version is missing above, the filename may be available from:
+    # https://resources.itrsgroup.com/downloads
+
+    ### UNSUPPORTED VERSIONS ###
+
+    # curl $OP5URL/op5-monitor-6.3.3-20140912.tar.gz
+
+    check_for_monitor_file
+    if ((fileexists == "false")); then
+    log "No Monitor file found, fixing that: downloading $LATEST_FILENAME."
+
+    cd /vagrant && curl $OP5URL$LATEST_FILENAME &>/dev/null
+    fi
+fi
+
+check_for_monitor_file
+
+if ((fileexists == "true")); then
+    log "Unpacking the Monitor file."
     cd /vagrant && tar xvf *onitor*.gz &>/dev/null
-    echo $($TIMES) $PREFX "Unpack complete. Executing non-interactive OP5 installation. Expect no output, this will take some time."
-    cd *onitor*/ && ./install.sh --noninteractive &>/dev/null
+    log "Executing non-interactive \
+OP5 installation. This will take some time (~10min)."
+    cd *onitor* && ./install.sh --noninteractive &>/dev/null
+    log "Installation process finished."
+else
+    log "No monitor file found! Exiting."
+    exit 1
 fi
 
 if ls /var/lib/yum/transaction-* 1> /dev/null 2>&1; then
-    # there seem to be unfinished transactions. it's unclear why this happens, but if there are,
-    # we should probably finish them before moving on.
-    echo $($TIMES) $PREFX "Unfinished transactions were found. Telling yum to finish them."
-    echo $($TIMES) $PREFX "Preserving yum log as /var/log/yum.log.gz first."
+    log "Sanity check: Unfinished yum transactions!"
+    log "Sanity check: Preserving yum log as /var/log/yum.log.gz first."
     gzip -9 /var/log/yum.log
-    echo $($TIMES) $PREFX "Completing transaction."
-    yum-complete-transaction
+    log "Sanity check: Completing transactions."
+    yum-complete-transaction &>/dev/null && log "Sanity check: Done."
 else
-    echo $($TIMES) $PREFX "No unfinished transactions, yum seems to be done."
+    log "Sanity check: No unfinished transactions (that's good)."
 fi
 
-echo -e "\n\n"
+log "Using *.lic file from /vagrant, if you put it there."
 
-# Any .lic file in /vagrant ? If so, use it.
-echo $($TIMES) $PREFX "If a license file exists, it will now be moved into place:"
-mv -v /vagrant/*.lic /etc/op5license/
+if [[ -f /vagrant/op5license.lic ]]
+then
+    mv -v /vagrant/op5license.lic /etc/op5license/
+    log "License file moved into place."
+else
+    log "No license file found, this will be a trial run."
+fi
 
-echo $($TIMES) $PREFX "Time remaining for your OP5 license (check_op5_license):"
-/opt/plugins/check_op5_license -w1 -c1 -T d
+log "Time remaining for your OP5 license (check_op5_license):"
+log "$(/opt/plugins/check_op5_license -w1 -c1 -T d)"
 
-echo "[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]"
-echo $($TIMES) $PREFX "The provision script for this guest has finished."
-echo $($TIMES) $PREFX "You should be able to access this Monitor instance on:"
-echo $($TIMES) $PREFX "https://localhost:4436 (Centos6) or https://localhost:4437 (Centos7)."
-echo $($TIMES) $PREFX "For more information on guest configuration, see the vagrant file. Have fun!"
-echo $($TIMES) $PREFX "If Monitor/Apache/etc needs root, you may want to take note of /etc/pam.d/su "
-echo $($TIMES) $PREFX "On some boxes, only the 'vagrant' user is allowed to sudo."
-echo $($TIMES) $PREFX "DONE. OP5 Vagrant script signing off. Good night."
+if ((phpdebug == "true")); then
+
+    log "PHP debug tools will now be installed, as requested."
+    yum install php-devel -y &>/dev/null
+    log "php-devel installed via yum."
+    log "Installing group 'Development Tools' via yum (be patient)."
+    yum groupinstall "Development Tools" -y &>/dev/null
+    log "Development tools have been installed. Starting xdebug download."
+    cd /tmp && curl -O https://xdebug.org/files/xdebug-2.4.1.tgz &>/dev/null
+    log "xdebug downloaded."
+    tar xvf xdebug-2.4.1.tgz &>/dev/null && cd xdebug* || exit 1
+    log "Running phpize."
+    phpize
+    log "Configuring xdebug and running make + install."
+    ./configure --enable-xdebug &>/dev/null && \
+    make &>/dev/null && make install &>/dev/null 
+    
+    if [[ -f /usr/lib64/php/modules/xdebug.so ]]
+        then
+        log "/usr/lib64/php/modules/xdebug.so present and accounted for."
+    else
+        log "xdebug.so is not in /usr/lib64/php/modules/ !!!"
+    fi
+
+    log "Amending config block to end of php.ini (idekey = VSCODE)"
+
+cat >> /etc/php.ini <<EOF
+zend_extension="/usr/lib64/php/modules/xdebug.so"
+xdebug.remote_enable = 1
+xdebug.remote_autostart = 1
+xdebug.remote_port = 9000
+xdebug.idekey = VSCODE
+EOF
+
+    log "Setting up a file at monitor/phpinfo.php for your convenience."
+
+cat >> /opt/monitor/op5/ninja/phpinfo.php <<EOF
+<?php
+phpinfo();
+?>
+EOF
+
+    log "Trying to copy vagrants authorized_keys to root. Use:"
+    log ".vagrant/machines/centos7/virtualbox/private_key to SSH in as root."
+
+    if [[ -f /home/vagrant/.ssh/authorized_keys ]]
+        then
+        cd /home/vagrant/.ssh/
+        mkdir -p /root/.ssh && cp -fv authorized_keys /root/.ssh
+    else
+        log "Oh no! There's no authorized_keys to copy :("
+    fi
+
+    log "Done."
+    log "You probably want to add this machine to .ssh/config"
+    log "Hints:"
+    log "IdentityFile [vagrants private_key]"
+    log "For keys with passphrases (probably not Vagrant) you also want:"
+    log "ssh-add ~/.ssh/your_private_key"
+
+fi
+
+echo "[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]"
+echo "[>>>>>>>>>>>>>>>>>>>>>>>>> F I N I S H E D >>>>>>>>>>>>>>>>>>>>>>>>>>>>>]"
+echo "[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]"
+log "The provision script for this guest has finished."
+log "You should be able to access Monitor on:"
+log "https://localhost:4436 (EL6) or https://localhost:4437 (EL7)."
+log "If a user needs root, you may want to take note of /etc/pam.d/su since"
+log "on some boxes, only the 'vagrant' user is allowed to sudo."
+printf "\n\n"
+log "WARNING: You may get HSTS errors in some browsers. To work around this, "
+log "set up the hostname of the box in your host file, or use another browser."
